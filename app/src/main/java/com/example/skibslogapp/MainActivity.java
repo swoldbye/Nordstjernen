deb80d15cba2dfffb138764971bd0fe7bc9e7370
Simon Woldbye-Lyng <s180943@student.dtu.dk>
2019-10-18 14:18:34 +0200

    removed action-bar from 'MainActivity'

--- app/src/main/java/com/example/skibslogapp/MainActivity.java
+++ app/src/main/java/com/example/skibslogapp/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         //Vind Retning
         nordButton = (Button) findViewById(R.id.nordButton);
