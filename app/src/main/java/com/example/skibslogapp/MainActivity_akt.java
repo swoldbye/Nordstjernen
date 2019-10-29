d182f7a5eebb77824f9dab2224824ecb3c5a8a60
jacob <jacobriis86@hotmail.com>
2019-10-29 14:19:18 +0100

    left menu created

--- app/src/main/java/com/example/skibslogapp/MainActivity_akt.java
+++ app/src/main/java/com/example/skibslogapp/MainActivity_akt.java
@@ -1,22 +1,101 @@
 package com.example.skibslogapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 import com.example.skibslogapp.viewControl.OpretLog_frag;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity_akt extends AppCompatActivity {
+
+    private RelativeLayout relativeLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_draver);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        setToolbar();
+        configureNavigationDrawer();
 
         if (savedInstanceState == null){
             Fragment fragment = new OpretLog_frag();
             getSupportFragmentManager().beginTransaction().add(R.id.fragContainer, fragment).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.empty, menu);
+        return true;
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_white_18dp);
+    }
+
+
+
+    private void configureNavigationDrawer(){
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.leftMenu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                int itemid = menuItem.getItemId();
+
+                //Input functionality for menu items
+
+                if (itemid == R.id.nav_help){
+
+                }else if (itemid == R.id.nav_send){
+
+                }else if (itemid == R.id.nav_about){
+
+                }else if (itemid == R.id.nav_email){
+
+                }
+                if (fragment != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragContainer, fragment);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        switch(itemId) {
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            // manage other entries if you have it ...
+        }
+        return true;
     }
 }
