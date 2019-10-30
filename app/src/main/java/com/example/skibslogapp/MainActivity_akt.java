4be0f6a55aeb483a70a1adf208b1b2e04b45ad2d
jacob <jacobriis86@hotmail.com>
2019-10-30 10:43:49 +0100

    funtionalitet til forskellige leftmenu elementer

--- app/src/main/java/com/example/skibslogapp/MainActivity_akt.java
+++ app/src/main/java/com/example/skibslogapp/MainActivity_akt.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -15,7 +16,9 @@ import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.skibslogapp.viewControl.LogOversigt_frag;
 import com.example.skibslogapp.viewControl.OpretLog_frag;
+import com.example.skibslogapp.viewControl.TogtOversigt_frag;
 import com.google.android.material.navigation.NavigationView;
 
 /**
@@ -25,6 +28,13 @@ public class MainActivity_akt extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    OpretLog_frag opretLog_frag;
+    TogtOversigt_frag togtOversigt_frag;
+    LogOversigt_frag logOversigt_frag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +72,11 @@ public class MainActivity_akt extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                opretLog_frag = new OpretLog_frag();
+                logOversigt_frag = new LogOversigt_frag();
+                togtOversigt_frag = new TogtOversigt_frag();
+
                 Fragment fragment = null;
                 int itemid = menuItem.getItemId();
 
@@ -70,6 +85,12 @@ public class MainActivity_akt extends AppCompatActivity {
                 if (itemid == R.id.nav_opret_togt){
 
                 }else if (itemid == R.id.nav_togt_oversigt){
+                    fragmentManager = MainActivity_akt.this.getSupportFragmentManager();
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragContainer, togtOversigt_frag);
+                    fragmentTransaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
 
                 }else if (itemid == R.id.nav_opret_etape){
 
@@ -77,17 +98,29 @@ public class MainActivity_akt extends AppCompatActivity {
 
                 }else if (itemid == R.id.nav_opret_log){
                     Toast.makeText(MainActivity_akt.this,"Hej fra toast",Toast.LENGTH_LONG).show();
-
-                }else if (itemid == R.id.nav_log_oversigt){
-
-                }
-                if (fragment != null) {
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.fragContainer, fragment);
-                    transaction.commit();
+                    fragmentManager = MainActivity_akt.this.getSupportFragmentManager();
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragContainer, opretLog_frag);
+                    fragmentTransaction.commit();
                     drawerLayout.closeDrawers();
                     return true;
+
+                }else if (itemid == R.id.nav_log_oversigt){
+                    fragmentManager = MainActivity_akt.this.getSupportFragmentManager();
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragContainer, logOversigt_frag);
+                    fragmentTransaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+
                 }
+//                if (fragment != null) {
+//                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                    transaction.replace(R.id.fragContainer, fragment);
+//                    transaction.commit();
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
                 return false;
             }
         });
@@ -107,7 +140,7 @@ public class MainActivity_akt extends AppCompatActivity {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
-            // manage other entries if you have it ...
+            // Indsæt flere entries, hvis der er...
         }
         return true;
     }
