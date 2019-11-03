903fcce8202791fc4d2ce87c14936b361f7670a0
Simon Woldbye-Lyng <s180943@student.dtu.dk>
2019-11-03 19:46:59 +0100

    Added files needed for first prototype.

---
+++ app/src/main/java/com/example/skibslogapp/PostActivity.java
@@ -0,0 +1,49 @@
+package com.example.skibslogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.example.skibslogapp.Model.LogInstans;
+
+public class PostActivity extends AppCompatActivity implements PostOversigt.OnPostOversigtListener, MainActivity.OnMainActivityListener {
+
+    MainActivity mainActivity = new MainActivity();
+    PostOversigt postOversigt = new PostOversigt();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        addOversigtFrag();
+    }
+
+
+
+
+    private void addOversigtFrag(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.postOversigtContainerFrame, postOversigt).commit();
+    }
+
+    @Override
+    public void hideOpretPost() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.hide(mainActivity).commit();
+    }
+
+    @Override
+    public void showOpretPost() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (mainActivity.isAdded()) {
+            ft.show(mainActivity).commit();
+        } else {
+            ft.add(R.id.opretPostContainerFrame, mainActivity).commit();
+        }
+    }
+
+    @Override
+    public void updateList(LogInstans nyeste) {
+        postOversigt.setList(nyeste);
+    }
+}
