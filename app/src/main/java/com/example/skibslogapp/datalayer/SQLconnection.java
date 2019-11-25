d533e8306027d71330c67a6a01597cbe7b567454
claeslindhardt1 <claeslindhardt@gmail.com>
2019-11-25 15:53:02 +0100

    made some simpel SQL for the SQLconnection, so that we can start testing weather the data model acts correct when we want to save an object

--- app/src/main/java/com/example/skibslogapp/datalayer/SQLconnection.java
+++ app/src/main/java/com/example/skibslogapp/datalayer/SQLconnection.java
@@ -1,15 +1,23 @@
 package com.example.skibslogapp.datalayer;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
 //db.execSQL();
 
 public class SQLconnection {
-    SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("Todo: fix getFilesDir()"+"/database.db",null);
+    //in the future consider implementing a getFilesDir() to make sure the db is put where you want it to be.
+    SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("/database.db",null);
 
-    public void OpretTabel(String tabelNavn){
+    public void OpretTogt(String tabelNavn, String togtNavn){
+        //Oprettelse af SQL tabel til til togted
         db.execSQL("DROP TABLE IF EXISTS "+tabelNavn+";");
-        db.execSQL("CREATE TABLE "+tabelNavn+" (_id INTEGER PRIMARY KEY AUTOINCREMENT ) ");
+        db.execSQL("CREATE TABLE "+tabelNavn+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, nanv TEXT NOT NULL) ");
+
+        //indsprøjtning af kendte informationer til db'en.
+        ContentValues togt = new ContentValues();
+        togt.put("navn", togtNavn);
+        db.insert(tabelNavn, null, togt);
 
     }
 
