82cfebfcad54e28f1e44e93aecf3c8369543f93e
maltebp <maltebp94@gmail.com>
2020-01-11 09:44:52 +0100

    Created coordinate convert

---
+++ app/src/main/java/com/example/skibslogapp/model/CoordinateConverter.java
@@ -0,0 +1,38 @@
+package com.example.skibslogapp.model;
+
+public class CoordinateConverter {
+
+    /**
+     * Convert longitude from format 29.3994  to  29°23.96'N
+     */
+    public static String toLongitudeString(double coordinate){
+        int degrees = (int) coordinate;
+        double minutes = (coordinate-degrees)*60;
+        String direction = minutes < 0 ? "S" : "N";
+        return String.format("%d\u00B0%.2f'%s", degrees, minutes, direction);
+    }
+
+    /**
+     * Convert latitude from format 29.3994  to  29°23.96'N
+     */
+    public static String toLatitudeString(double coordinate){
+        int degrees = (int) coordinate;
+        double minutes = (coordinate-degrees)*60;
+        String direction = minutes < 0 ? "W" : "E";
+        return String.format("%d\u00B0%.2f'%s", degrees, minutes, direction);
+    }
+
+
+    private static void print(double original, String dmm){
+        System.out.println( String.format("%.4f  -> %s",original,dmm));
+    }
+
+    public static void main(String[] args) {
+        double lon = 29.39945;
+
+
+        print(lon, toLongitudeString(lon));
+        print(lon, toLatitudeString(lon));
+
+    }
+}
