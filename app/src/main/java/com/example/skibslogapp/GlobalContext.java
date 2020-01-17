37269a42ce77d7c4da1221e2ea09353986da15c6
maltebp <maltebp94@gmail.com>
2020-01-17 13:29:31 +0100

    Documentation for GlobalContext

--- app/src/main/java/com/example/skibslogapp/GlobalContext.java
+++ app/src/main/java/com/example/skibslogapp/GlobalContext.java
@@ -3,6 +3,12 @@ package com.example.skibslogapp;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ * A global static way of getting the application context.
+ *
+ * Use the static 'get()' method instead of passing the context
+ * or activity/fragments around, if someone needs the context.
+ */
 public class GlobalContext extends Application {
     private static Context context = null;
 
@@ -12,6 +18,7 @@ public class GlobalContext extends Application {
         context = getApplicationContext();
     }
 
+    /** Retrieves the application context */
     public static Context get(){
         return context;
     }
