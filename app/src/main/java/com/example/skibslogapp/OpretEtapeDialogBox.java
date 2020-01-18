3b880ceff17a02d426ac9e88aa3120bb42306222
AndreasBGJensen <bgandreas@hotmail.com>
2020-01-18 10:01:01 +0100

    Add: add and delete to beseatning Liste

--- app/src/main/java/com/example/skibslogapp/OpretEtapeDialogBox.java
+++ app/src/main/java/com/example/skibslogapp/OpretEtapeDialogBox.java
@@ -6,16 +6,25 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.skibslogapp.datalayer.local.EtapeDAO;
 import com.example.skibslogapp.model.Etape;
 import com.example.skibslogapp.model.Togt;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +36,9 @@ public class OpretEtapeDialogBox extends AppCompatDialogFragment implements View
     private Togt togt;
     private View addButton;
     private List<String> besaetningList = new ArrayList<>();
+    RecyclerView besaetning;
+
+
 
     public OpretEtapeDialogBox(Togt togt) {
         this.togt = togt;
@@ -35,7 +47,7 @@ public class OpretEtapeDialogBox extends AppCompatDialogFragment implements View
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-
+        besaetningList = new ArrayList<>();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -62,6 +74,9 @@ public class OpretEtapeDialogBox extends AppCompatDialogFragment implements View
                     }
                 });
 
+        besaetningList.add("ASDKASKDH");
+        besaetningList.add("ASDKASKDH");
+
         editSkipper = view.findViewById(R.id.inputSkipper);
         editStartDest = view.findViewById(R.id.inputStartDest);
         navnIndput = view.findViewById(R.id.navnIndput);
@@ -69,13 +84,75 @@ public class OpretEtapeDialogBox extends AppCompatDialogFragment implements View
         addButton = view.findViewById(R.id.addButton);
         addButton.setOnClickListener(this);
 
+        besaetning = view.findViewById(R.id.besaetningList);
+
+        besaetning.setLayoutManager(new LinearLayoutManager(getActivity()));
+        besaetning.setAdapter(adapter);
+
+
+
+
         return builder.create();
 
     }
+
+    RecyclerView.Adapter adapter = new RecyclerView.Adapter<ListeelemViewholder>() {
+        @Override
+        public int getItemCount()  {
+            return besaetningList.size();
+        }
+
+        @Override
+        public ListeelemViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.opret_etape_dialog_box_list_element, parent, false);
+            ListeelemViewholder vh = new ListeelemViewholder(view);
+            vh.nameElement = view.findViewById(R.id.listElementBesaetning);
+            vh.cancleBeseatning = view.findViewById(R.id.cancleBeseatning);
+            vh.cancleBeseatning.setOnClickListener(vh);
+            vh.cancleBeseatning.setBackgroundResource(android.R.drawable.list_selector_background);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ListeelemViewholder vh, int position) {
+            vh.nameElement.setText(besaetningList.get(position));
+            vh.cancleBeseatning.setImageResource(android.R.drawable.ic_delete);
+        }
+    };
+
 
     @Override
     public void onClick(View v) {
         besaetningList.add(navnIndput.getText().toString());
         System.out.println(navnIndput.getText().toString());
+        adapter.notifyDataSetChanged();
     }
+
+
+
+
+    class ListeelemViewholder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView nameElement;
+        ImageView cancleBeseatning;
+
+        public ListeelemViewholder(View itemView) {
+            super(itemView);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            final int position = getAdapterPosition();
+            final String besaetningsmedlem = besaetningList.get(position);
+
+            if(v ==cancleBeseatning) {
+
+                besaetningList.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+        }
+    }
+
 }
+
+
