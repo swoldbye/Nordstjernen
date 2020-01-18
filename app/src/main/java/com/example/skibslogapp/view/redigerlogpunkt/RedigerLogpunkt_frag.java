13f9c5c34fc096b1f717f26667dc748da9aca176
KNA <kristian1711@gmail.com>
2020-01-18 13:20:14 +0100

    RedigerLogpunkt_frag can now update the information of the page Fixed Logpunkt.java to have -1 where the values should be "empty"

--- app/src/main/java/com/example/skibslogapp/view/redigerlogpunkt/RedigerLogpunkt_frag.java
+++ app/src/main/java/com/example/skibslogapp/view/redigerlogpunkt/RedigerLogpunkt_frag.java
@@ -12,7 +12,17 @@ import com.example.skibslogapp.R;
 import com.example.skibslogapp.model.Logpunkt;
 
 public class RedigerLogpunkt_frag extends Fragment {
-
+    private TextView    time,
+                        latitude,
+                        longitude,
+                        windDirection,
+                        windSpeed,
+                        currentDirection,
+                        currentSpeed,
+                        sailsOrRowers,
+                        sails,
+                        course,
+                        note;
     private Logpunkt logpunkt;
 
     public RedigerLogpunkt_frag(Logpunkt logpunkt){
@@ -23,6 +33,40 @@ public class RedigerLogpunkt_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rediger_logpunkt, container, false);
         ((TextView) view.findViewById(R.id.test_text)).setText(logpunkt.toString());
+
+        time = view.findViewById(R.id.editLogTimeInfo);
+        latitude = view.findViewById(R.id.editLogLatitude);
+        longitude = view.findViewById(R.id.editLogLongitude);
+        windDirection = view.findViewById(R.id.editLogWindDirectionInfo);
+        windSpeed = view.findViewById(R.id.editLogWindSpeedInfo);
+        currentDirection = view.findViewById(R.id.editLogWaterCurrentDirectionInfo);
+        currentSpeed = view.findViewById(R.id.editLogWaterCurrentSpeedInfo);
+        sailsOrRowers = view.findViewById(R.id.editLogSailsOrRowersInfo);
+        sails = view.findViewById(R.id.editLogSailsPositionInfo);
+        course = view.findViewById(R.id.editLogCourseInfo);
+        note = view.findViewById(R.id.editLogNoteInfo);
+
+        updateInformation();
+
         return view;
+    }
+
+    private void updateInformation() {
+        time.setText(logpunkt.getTimeString() != null && !logpunkt.getTimeString().equals("") ? logpunkt.getTimeString() : "-");
+        latitude.setText(logpunkt.getPosition() != null && !logpunkt.getPosition().getBreddegradString().equals("") ?
+                logpunkt.getPosition().getBreddegradString() : "-");
+        longitude.setText(logpunkt.getPosition() != null && !logpunkt.getPosition().getLaengdegradString().equals("") ?
+                logpunkt.getPosition().getLaengdegradString() : "-");
+        windDirection.setText(logpunkt.getVindretning() != null && !logpunkt.getVindretning().equals("") ? logpunkt.getVindretning() : "-");
+        windSpeed.setText(logpunkt.getVindhastighed() >= 0 ? String.valueOf(logpunkt.getVindhastighed()) : "-");
+        currentDirection.setText(logpunkt.getStroemRetning() != null && !logpunkt.getStroemRetning().equals("") ? logpunkt.getStroemRetning() : "-");
+        currentSpeed.setText(logpunkt.getStroemhastighed() >= 0 ? String.valueOf(logpunkt.getStroemhastighed()) : "-");
+        String sailsOrRowersString = logpunkt.getSejlstillingString() != null && !logpunkt.getSejlstillingString().equals("") ?
+                logpunkt.getSejlstillingString() : "";
+        sailsOrRowersString.concat(logpunkt.getRoere() >= 0 ? Integer.toString(logpunkt.getRoere()) : "");
+        sailsOrRowers.setText(sailsOrRowersString.length() != 0 ? sailsOrRowersString : "-");
+        sails.setText(logpunkt.getSejloeringString() != null && !logpunkt.getSejloeringString().equals("") ? logpunkt.getSejloeringString() : "-");
+        course.setText(logpunkt.getKursString() != null && !logpunkt.getKursString().equals("") ? logpunkt.getKursString() : "-");
+        note.setText(logpunkt.getNote() != null && !logpunkt.getNote().equals("") ? logpunkt.getNote() : "-");
     }
 }
