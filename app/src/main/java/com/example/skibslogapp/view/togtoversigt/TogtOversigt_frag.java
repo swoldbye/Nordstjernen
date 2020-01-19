92593de18a0940221ebb4bf6d080011763e102b2
jacob <jacobriis86@hotmail.com>
2020-01-19 14:00:20 +0100

    HVis der ingen oprettede togter er står der en text som hjælper en med hvordan man skal oprette togter

--- app/src/main/java/com/example/skibslogapp/view/togtoversigt/TogtOversigt_frag.java
+++ app/src/main/java/com/example/skibslogapp/view/togtoversigt/TogtOversigt_frag.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -26,9 +27,16 @@ import java.util.List;
  */
 public class TogtOversigt_frag extends Fragment implements View.OnClickListener {
 
+    private List<Togt> togtList;
+    private TogtDAO togtDAO;
+    private TextView ingenTogtText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_togt_oversigt, container, false);
+
+        togtDAO = new TogtDAO(getActivity());
+        togtList = togtDAO.getTogter();
 
         View opretTogt = view.findViewById(R.id.opretTogtBtn);
         opretTogt.setOnClickListener(this);
@@ -36,13 +44,25 @@ public class TogtOversigt_frag extends Fragment implements View.OnClickListener 
         TogtDAO togtDAO = new TogtDAO(getContext());
         List<Togt> togtList = togtDAO.getTogter();
 
-        RecyclerView recyclerView = view.findViewById(R.id.togtRecycView);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
-        RecyclerView.Adapter adapter = new TogtListAdapter(togtList,getContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-        recyclerView.smoothScrollToPosition(adapter.getItemCount()-1);
+        ingenTogtText = view.findViewById(R.id.ingenTogtText);
+
+        if (togtList.size() == 0){
+            ingenTogtText.setText("Der er ingen oprettede togter.\n " +
+                    "Hvis der skal oprettes et togt tryk på 'Opret Togt'");
+            view.findViewById(R.id.togtRecycView).setVisibility(View.GONE);
+            view.findViewById(R.id.togtOversigtUdenTogter).setVisibility(View.VISIBLE);
+        }else{
+            view.findViewById(R.id.togtOversigtUdenTogter).setVisibility(View.GONE);
+            view.findViewById(R.id.togtRecycView).setVisibility(View.VISIBLE);
+            RecyclerView recyclerView = view.findViewById(R.id.togtRecycView);
+            recyclerView.setHasFixedSize(true);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+            RecyclerView.Adapter adapter = new TogtListAdapter(togtList,getContext());
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(adapter);
+            recyclerView.smoothScrollToPosition(adapter.getItemCount()-1);
+        }
+
         return view;
     }
 
