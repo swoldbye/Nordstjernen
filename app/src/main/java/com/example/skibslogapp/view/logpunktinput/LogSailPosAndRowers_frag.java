ed5456e9d58a234c00d5763e4fb52338fa04c14d
KNA <kristian1711@gmail.com>
2020-01-22 21:21:59 +0100

    LogSailPosAndRowers_frag cleaned and commented

--- app/src/main/java/com/example/skibslogapp/view/logpunktinput/LogSailPosAndRowers_frag.java
+++ app/src/main/java/com/example/skibslogapp/view/logpunktinput/LogSailPosAndRowers_frag.java
@@ -16,6 +16,9 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.skibslogapp.R;
 import com.example.skibslogapp.utility.KingButton;
 
+/**
+ * Fragment to set the position of the sails and number of rowers with user inputs.
+ */
 public class LogSailPosAndRowers_frag extends Fragment implements View.OnClickListener {
     private KingButton læ, ag, bi, fo, ha;
     private EditText numberOfRowers;
@@ -25,7 +28,7 @@ public class LogSailPosAndRowers_frag extends Fragment implements View.OnClickLi
         View view = inflater.inflate(R.layout.logpunktinput_sejlstilling, container, false);
         logVM = ViewModelProviders.of(getActivity()).get(LogViewModel.class);
 
-        //Sejlstilling
+        //Position of sail
         læ = view.findViewById(R.id.læ);
         ag = view.findViewById(R.id.ag);
         bi = view.findViewById(R.id.bi);
@@ -47,8 +50,8 @@ public class LogSailPosAndRowers_frag extends Fragment implements View.OnClickLi
         bi.createRelation(ha);
         fo.createRelation(ha);
 
-        //Antal Roere
-        numberOfRowers = (EditText) view.findViewById(R.id.antalRoereEditText);
+        //Number of rowers
+        numberOfRowers = view.findViewById(R.id.antalRoereEditText);
         numberOfRowers.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -70,27 +73,6 @@ public class LogSailPosAndRowers_frag extends Fragment implements View.OnClickLi
         updateViewInfo();
 
         return view;
-    }
-
-    private void updateViewInfo() {
-        switch(logVM.getSailPosition().toLowerCase()) {
-            case "læ":
-                læ.kingSelected();
-                break;
-            case "ag":
-                ag.kingSelected();
-                break;
-            case "bi":
-                bi.kingSelected();
-                break;
-            case "fo":
-                fo.kingSelected();
-                break;
-            case "ha":
-                ha.kingSelected();
-                break;
-        }
-        numberOfRowers.setText(logVM.getCurrRowers() >= 0 ? Integer.toString(logVM.getCurrRowers()) : "");
     }
 
     @Override
@@ -120,22 +102,50 @@ public class LogSailPosAndRowers_frag extends Fragment implements View.OnClickLi
             if(v.isSelected()) logVM.setSailPosition(ha.getText().toString());
             else logVM.setSailPosition("");
         }
-        resetRowersInput();
+        resetRowersInput(); //Sets the number of rowers as none
     }
 
+    /**
+     * Updates the text of the view elements
+     */
+    private void updateViewInfo() {
+        switch(logVM.getSailPosition().toLowerCase()) {
+            case "læ":
+                læ.kingSelected();
+                break;
+            case "ag":
+                ag.kingSelected();
+                break;
+            case "bi":
+                bi.kingSelected();
+                break;
+            case "fo":
+                fo.kingSelected();
+                break;
+            case "ha":
+                ha.kingSelected();
+                break;
+        }
+        numberOfRowers.setText(logVM.getCurrRowers() >= 0 ? Integer.toString(logVM.getCurrRowers()) : "");
+    }
+
+    /**
+     * Sets sail position as not selected
+     */
     private void resetSailPosInput() {
         if(logVM.getCurrRowers() >= 0 && logVM.getSailPosition().length() > 0) {
-            {
-                logVM.setSailPosition("");
-                læ.kingDeselected();
-                ag.kingDeselected();
-                bi.kingDeselected();
-                fo.kingDeselected();
-                ha.kingDeselected();
-            }
+            logVM.setSailPosition("");
+            læ.kingDeselected();
+            ag.kingDeselected();
+            bi.kingDeselected();
+            fo.kingDeselected();
+            ha.kingDeselected();
         }
     }
 
+    /**
+     * Resets the number of rowers as none
+     */
     private void resetRowersInput() {
         logVM.setCurrRowers(-1);
         numberOfRowers.setText("");
