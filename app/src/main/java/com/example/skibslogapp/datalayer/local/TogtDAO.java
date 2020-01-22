43357851f5b522a37dab83710b75bd102ee6542e
jacob <jacobriis86@hotmail.com>
2020-01-22 13:34:24 +0100

    SQLiteConnector and TogtDAO formatted

--- app/src/main/java/com/example/skibslogapp/datalayer/local/TogtDAO.java
+++ app/src/main/java/com/example/skibslogapp/datalayer/local/TogtDAO.java
@@ -21,11 +21,10 @@ public class TogtDAO {
 
     private static LinkedList<TogtObserver> togtObservers = new LinkedList<>();
 
-    public TogtDAO(Context context){
+    public TogtDAO(Context context) {
         connector = new SQLiteConnector(context);
         this.context = context;
     }
-
 
     /**
      * Add a Togt to the database, using the object field values.
@@ -34,15 +33,16 @@ public class TogtDAO {
      *
      * @param togt The Togt to save to the database
      */
-    public void addTogt(Togt togt){
+    public void addTogt(Togt togt) {
         SQLiteDatabase database = connector.getWritableDatabase();
 
         ContentValues row = new ContentValues();
         row.put("name", togt.getName());
 
-        if( togt.getSkib() != null ) row.put("skib", togt.getSkib());
-        if( togt.getSkipper() != null ) row.put("skipper", togt.getSkipper());
-        if( togt.getStartDestination() != null ) row.put("startDestination", togt.getStartDestination());
+        if (togt.getSkib() != null) row.put("skib", togt.getSkib());
+        if (togt.getSkipper() != null) row.put("skipper", togt.getSkipper());
+        if (togt.getStartDestination() != null)
+            row.put("startDestination", togt.getStartDestination());
 
         long id = database.insert("togter", null, row);
         togt.setId(id);
@@ -51,33 +51,31 @@ public class TogtDAO {
         database.close();
     }
 
-
     /**
      * Loads all Togt objects from the the local database.
      *
      * @return A list of the loaded Togt objects. The list is empty (not null) if no Togt exists in the database.
      */
-    public List<Togt> getTogter(){
+    public List<Togt> getTogter() {
 
         // Create cursor for database rows
         SQLiteDatabase database = connector.getReadableDatabase();
-        Cursor cursor = database.rawQuery("SELECT * FROM togter;", null );
+        Cursor cursor = database.rawQuery("SELECT * FROM togter;", null);
         //Cursor cursor = database.query("togter", null, null, null,null, null, null);
         LinkedList<Togt> togter = new LinkedList<>();
 
-
         // Load data from each row
-        while( cursor.moveToNext() ){
+        while (cursor.moveToNext()) {
             Togt togt = new Togt(cursor.getString(cursor.getColumnIndex("name")));
-            togt.setId(cursor.getInt( cursor.getColumnIndex("id")));
+            togt.setId(cursor.getInt(cursor.getColumnIndex("id")));
 
-            if( !cursor.isNull(cursor.getColumnIndex("skib")))
+            if (!cursor.isNull(cursor.getColumnIndex("skib")))
                 togt.setSkib(cursor.getString(cursor.getColumnIndex("skib")));
 
-            if( !cursor.isNull(cursor.getColumnIndex("skipper")))
+            if (!cursor.isNull(cursor.getColumnIndex("skipper")))
                 togt.setSkipper(cursor.getString(cursor.getColumnIndex("skipper")));
 
-            if( !cursor.isNull(cursor.getColumnIndex("startDestination")))
+            if (!cursor.isNull(cursor.getColumnIndex("startDestination")))
                 togt.setStartDestination(cursor.getString(cursor.getColumnIndex("startDestination")));
             togter.add(togt);
         }
@@ -87,7 +85,6 @@ public class TogtDAO {
         return togter;
     }
 
-
     /**
      * Deletes a Togt from the local database, including all the Etaper and
      * Logpunkter for that Togt.
@@ -95,33 +92,32 @@ public class TogtDAO {
      * @param togt The Togt to be deleted (the method matches the ID of the Togt object with an ID in the database)
      */
     public void deleteTogt(Togt togt) throws DAOException {
-        if( !togtExists(togt) ){
+        if (!togtExists(togt)) {
             throw new DAOException(String.format(Locale.US, "Togt with id %d doesn't exist in database", togt.getId()));
         }
 
         // Deletes Etaper for the Togt
         EtapeDAO etapeDAO = new EtapeDAO(context);
-        for( Etape etape : etapeDAO.getEtaper(togt) ){
+        for (Etape etape : etapeDAO.getEtaper(togt)) {
             etapeDAO.deleteEtape(etape);
         }
 
         SQLiteDatabase database = connector.getReadableDatabase();
-        database.delete("togter", "id="+togt.getId(), null );
+        database.delete("togter", "id=" + togt.getId(), null);
 
         database.close();
     }
-
 
     /**
      * Retrieves a Togt based on the ID. Not meant to be used
      * outside of the class.
      */
-    public Togt getTogt(long id){
+    public Togt getTogt(long id) {
         SQLiteDatabase database = connector.getReadableDatabase();
-        Cursor cursor = database.rawQuery("SELECT * FROM togter WHERE id="+id+";", null);
+        Cursor cursor = database.rawQuery("SELECT * FROM togter WHERE id=" + id + ";", null);
 
         Togt togt = null;
-        while(cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             togt = new Togt(cursor.getString(1));
             togt.setId(cursor.getInt(0));
         }
@@ -131,29 +127,27 @@ public class TogtDAO {
         return togt;
     }
 
-
-    protected void togtUpdated(long id){
+    protected void togtUpdated(long id) {
         Togt togt = getTogt(id);
 
-        if( togt == null )
-            throw new DAOException("No Togt with ID "+id+" exists in the database");
+        if (togt == null)
+            throw new DAOException("No Togt with ID " + id + " exists in the database");
 
-        for( TogtObserver observer : togtObservers){
+        for (TogtObserver observer : togtObservers) {
             observer.onUpdate(togt);
         }
     }
-
 
     /**
      * Add an observer to be notified whenever a togt has been updated.
      *
      * @param observer The observer to be notified
      */
-    public static void addTogtObserver( TogtObserver observer ){
+    public static void addTogtObserver(TogtObserver observer) {
         togtObservers.add(observer);
     }
 
-    public static void removeTogtObserver( TogtObserver observer ) {
+    public static void removeTogtObserver(TogtObserver observer) {
         togtObservers.remove(observer);
     }
     // TODO: TOGT OBSERVER FUNCTIONALITY IS NOT UP TO DATE - either remove or update
@@ -165,19 +159,21 @@ public class TogtDAO {
      * @param togt The Togt to check if exists in the database
      * @return True if it exists, false if not
      */
-    public boolean togtExists(Togt togt){
-        if( togt.getId() == -1 ) return false;
+    public boolean togtExists(Togt togt) {
+        if (togt.getId() == -1) return false;
 
         SQLiteDatabase database = connector.getReadableDatabase();
-        Cursor cursor = database.rawQuery("SELECT * FROM togter WHERE id="+togt.getId()+";", null);
+        Cursor cursor = database.rawQuery("SELECT * FROM togter WHERE id=" + togt.getId() + ";", null);
         int rowCount = cursor.getCount();
         cursor.close();
         database.close();
         return rowCount > 0;
     }
 
-
-    public interface TogtObserver{
+    /**
+     *
+     */
+    public interface TogtObserver {
         void onUpdate(Togt togt);
     }
 
